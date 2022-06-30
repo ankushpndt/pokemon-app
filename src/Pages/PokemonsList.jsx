856fcd75ba76a1e3a263e7fcd34890a1e0feddd6
Ankush <ankushpndt@gmail.com>
2022-06-30 13:27:52 +0530

    fix: bug fix in pokemonList

--- src/Pages/PokemonsList.jsx
+++ src/Pages/PokemonsList.jsx
@@ -1,68 +1,70 @@
-import React, { useState } from "react";
-import { usePokemonList } from "../context/ListContext";
-import { v4 } from "uuid";
-import InfiniteScroll from "react-infinite-scroll-component";
-import PokemonDetails from "./PokemonDetails";
-import axios from "axios";
+import React, { useState } from 'react';
+import { usePokemonList } from '../context/ListContext';
+import { v4 } from 'uuid';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import PokemonDetails from './PokemonDetails';
+import axios from 'axios';
 
 const PokemonsList = () => {
-	const { filteredArr, page, setPage, setFilteredArr, setPokemons } =
-		usePokemonList();
-	const [hasMore, sethasMore] = useState(true);
-	const fetchPokemons = async () => {
-		const response = await axios.get(
-			`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${page}`
-		);
-		function pokemonDetailsFunc(result) {
-			result?.forEach(async (pokemon) => {
-				const response = await axios.get(
-					` https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-				);
+  const { filteredArr, page, setPage, setFilteredArr, setPokemons } =
+    usePokemonList();
+  const [hasMore, sethasMore] = useState(true);
+  const fetchPokemons = async () => {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon?limit=50&offset=${page}`
+    );
+    function pokemonDetailsFunc(result) {
+      result?.forEach(async (pokemon) => {
+        const response = await axios.get(
+          ` https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
 
-				setPokemons((currentList) => [...currentList, response?.data]);
-			});
-		}
-		pokemonDetailsFunc(response?.data?.results);
-		return response?.data?.results;
-	};
+        setPokemons((currentList) => [...currentList, response?.data]);
+      });
+    }
+    pokemonDetailsFunc(response?.data?.results);
+    return response?.data?.results;
+  };
 
-	const fetchData = async () => {
-		const pokemonsFromServer = await fetchPokemons();
+  const fetchData = async () => {
+    const pokemonsFromServer = await fetchPokemons();
 
-		setFilteredArr([...filteredArr, ...pokemonsFromServer]);
-		if (pokemonsFromServer.length === 0 || pokemonsFromServer.length < 50) {
-			sethasMore(false);
-		}
-		setPage(page + 1);
-	};
-	console.log({ filteredArr });
-	return (
-		<div className="list__of__pokemon">
-			<ul>
-				<InfiniteScroll
-					dataLength={filteredArr?.length}
-					next={fetchData}
-					hasMore={hasMore}
-					loader={<h4>Loading...</h4>}
-					style={{ display: "flex", flexWrap: "wrap" }}
-				>
-					{filteredArr?.map((pokemon) => {
-						return (
-							<div key={v4()} className="pokemon-container">
-								<PokemonDetails
-									id={pokemon?.id}
-									name={pokemon?.name}
-									image={pokemon?.sprites?.other?.dream_world?.front_default}
-									// type={pokemon?.types[0]?.type?.name}
-									stats={pokemon?.stats}
-								/>
-							</div>
-						);
-					})}
-				</InfiniteScroll>
-			</ul>
-		</div>
-	);
+    setFilteredArr([...filteredArr, ...pokemonsFromServer]);
+    if (pokemonsFromServer.length === 0 || pokemonsFromServer.length < 50) {
+      sethasMore(false);
+    }
+    setPage(page + 1);
+  };
+
+  return (
+    <div className='list__of__pokemon'>
+      <ul>
+        <InfiniteScroll
+          dataLength={filteredArr?.length}
+          next={fetchData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          style={{ display: 'flex', flexWrap: 'wrap' }}
+        >
+          {filteredArr?.map((pokemon) => {
+            return (
+              <div key={v4()} className='pokemon-container'>
+                <PokemonDetails
+                  id={pokemon?.id}
+                  name={pokemon?.name}
+                  image={pokemon?.sprites?.other?.dream_world?.front_default}
+                  type={
+                    pokemon?.types ? pokemon?.types[0]?.type?.name : undefined
+                  }
+                  stats={pokemon?.stats}
+                />
+              </div>
+            );
+          })}
+        </InfiniteScroll>
+      </ul>
+    </div>
+  );
 };
 
 export default PokemonsList;
--- src/context/ListContext.jsx
+++ src/context/ListContext.jsx
@@ -1,57 +1,57 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
-import axios from "axios";
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 const ListContext = createContext();
 export const ListProvider = ({ children }) => {
-	const [pokemons, setPokemons] = useState([]);
-	const [search, setSearch] = useState("");
-	const [filteredArr, setFilteredArr] = useState([...pokemons]);
-	// const [loading, setLoading] = useState(false);
-	// const [loadMore, setLoadMore] = useState(
-	// 	"https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
-	// );
-	const [page, setPage] = useState(50);
+  const [pokemons, setPokemons] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filteredArr, setFilteredArr] = useState([...pokemons]);
+  // const [loading, setLoading] = useState(false);
+  // const [loadMore, setLoadMore] = useState(
+  // 	"https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
+  // );
+  const [page, setPage] = useState(50);
 
-	useEffect(() => {
-		(async () => {
-			try {
-				const response = await axios.get(
-					`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`
-				);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`
+        );
 
-				// setLoadMore(response?.data?.next);
+        // setLoadMore(response?.data?.next);
 
-				function pokemonDetailsFunc(result) {
-					result?.forEach(async (pokemon) => {
-						const response = await axios.get(
-							` https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-						);
+        function pokemonDetailsFunc(result) {
+          result?.forEach(async (pokemon) => {
+            const response = await axios.get(
+              ` https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+            );
 
-						setPokemons((currentList) => [...currentList, response?.data]);
-					});
-				}
-				pokemonDetailsFunc(response?.data?.results);
-			} catch (err) {
-				console.log(err);
-			}
-		})();
-	}, []);
-	console.log({ pokemons });
-	return (
-		<ListContext.Provider
-			value={{
-				pokemons,
-				setPokemons,
-				filteredArr,
-				setFilteredArr,
-				search,
-				setSearch,
-				page,
-				setPage,
-			}}
-		>
-			{children}
-		</ListContext.Provider>
-	);
+            setPokemons((currentList) => [...currentList, response?.data]);
+          });
+        }
+        pokemonDetailsFunc(response?.data?.results);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  return (
+    <ListContext.Provider
+      value={{
+        pokemons,
+        setPokemons,
+        filteredArr,
+        setFilteredArr,
+        search,
+        setSearch,
+        page,
+        setPage,
+      }}
+    >
+      {children}
+    </ListContext.Provider>
+  );
 };
 
 export const usePokemonList = () => useContext(ListContext);
