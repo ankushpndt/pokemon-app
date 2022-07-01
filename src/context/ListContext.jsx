3a585f61433a818c91040dcd604707f4e5e93aae
Ankush <ankushpndt@gmail.com>
2022-07-01 10:56:33 +0530

    fix: bug fix for saved pokemons

--- src/context/ListContext.jsx
+++ src/context/ListContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import { useGoogleAuth } from './GoogleAuthContext';
 const ListContext = createContext();
 export const ListProvider = ({ children }) => {
   const [pokemons, setPokemons] = useState([]);
@@ -7,28 +8,33 @@ export const ListProvider = ({ children }) => {
   const [filteredArr, setFilteredArr] = useState([...pokemons]);
   const [page, setPage] = useState(50);
   const [savedPokemon, setSavedPokemon] = useState([]);
+  const { token } = useGoogleAuth();
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`
-        );
+        if (token) {
+          const response = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`
+          );
 
-        function pokemonDetailsFunc(result) {
-          result?.forEach(async (pokemon) => {
-            const response = await axios.get(
-              ` https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-            );
+          function pokemonDetailsFunc(result) {
+            result?.forEach(async (pokemon) => {
+              const response = await axios.get(
+                ` https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+              );
 
-            setPokemons((currentList) => [...currentList, response?.data]);
-          });
+              setPokemons((currentList) => [...currentList, response?.data]);
+            });
+          }
+          pokemonDetailsFunc(response?.data?.results);
+        } else {
+          setSavedPokemon([]);
         }
-        pokemonDetailsFunc(response?.data?.results);
       } catch (err) {
         console.log(err);
       }
     })();
-  }, []);
+  }, [token]);
 
   return (
     <ListContext.Provider
